2679cb4e3a78c5578a5bedbf0b850549b61be737
Bearnard Hibbins <bearnard@gmail.com>
2013-12-16 22:32:54 -0800

    bump luarocks

--- config/software/luarocks.rb
+++ config/software/luarocks.rb
@@ -1,9 +1,9 @@
 name "luarocks"
-version "2.0.12"
+version "2.1.1"
 
 dependency "nginx-openresty"
 
-source url: "http://luarocks.org/releases/luarocks-#{version}.tar.gz", md5: "a1bc938ddf835550917f0cb6964ea516"
+source url: "http://luarocks.org/releases/luarocks-#{version}.tar.gz", md5: "9df79d88f8abdc40f9da298296ae868c"
 
 relative_path "luarocks-#{version}"
 
@@ -12,7 +12,7 @@ build do
            "--prefix=#{install_dir}/embedded/luajit",
            "--lua-suffix=jit",
            "--with-lua=#{install_dir}/embedded/luajit",
-           "--with-lua-include=#{install_dir}/embedded/luajit/include/luajit-2.0",
+           "--with-lua-include=#{install_dir}/embedded/luajit/include/luajit-2.1",
            "--with-lua-lib=#{install_dir}/embedded/luajit/lib"
           ].join(" ")
 
