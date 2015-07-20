637a9b9d0f185a6a581d624c5a5ba1f7398152dd
Bearnard Hibbins <bearnard@gmail.com>
2015-07-20 14:51:52 +0200

    add software definition for bumped version

---
+++ config/software/openresty.rb
@@ -0,0 +1,78 @@
+#
+# Copyright 2012-2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "openresty"
+default_version "1.7.10.2"
+
+dependency "pcre"
+dependency "openssl"
+dependency "zlib"
+
+version("1.7.10.2") { source md5: "bca1744196acfb9e986f1fdbee92641e" }
+version("1.4.3.6") { source md5: "5e5359ae3f1b8db4046b358d84fabbc8" }
+
+source url: "http://openresty.org/download/ngx_openresty-#{version}.tar.gz"
+
+relative_path "ngx_openresty-#{version}"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+  env['PATH'] += "#{env['PATH']}:/usr/sbin:/sbin"
+
+  configure = [
+    "./configure",
+    "--prefix=#{install_dir}/embedded",
+    "--sbin-path=#{install_dir}/embedded/sbin/nginx",
+    "--conf-path=#{install_dir}/embedded/conf/nginx.conf",
+    "--with-http_ssl_module",
+    "--with-debug",
+    "--with-http_stub_status_module",
+    # Building Nginx with non-system OpenSSL
+    # http://www.ruby-forum.com/topic/207287#902308
+    "--with-ld-opt=\"-L#{install_dir}/embedded/lib -Wl,-rpath,#{install_dir}/embedded/lib -lssl -lcrypto -ldl -lz\"",
+    "--with-cc-opt=\"-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include\"",
+    # Options inspired by the OpenResty Cookbook
+    '--with-md5-asm',
+    '--with-sha1-asm',
+    '--with-pcre-jit',
+    '--with-luajit',
+    '--without-http_ssi_module',
+    '--without-mail_smtp_module',
+    '--without-mail_imap_module',
+    '--without-mail_pop3_module',
+    '--with-ipv6',
+    # AIO support define in Openresty cookbook. Requires Kernel >= 2.6.22
+    # Ubuntu 10.04 reports: 2.6.32-38-server #83-Ubuntu SMP
+    # However, they require libatomic-ops-dev and libaio
+    #'--with-file-aio',
+    #'--with-libatomic'
+  ]
+
+  # OpenResty 1.7 + RHEL5 Fixes:
+  # According to https://github.com/openresty/ngx_openresty/issues/85, OpenResty
+  # fails to compile on RHEL5 without the "--with-luajit-xcflags='-std=gnu99'" flags
+  if (version.to_f >= 1.7) &&                          # '1.7.7.2'.to_f evaluates to 1.7
+     (ohai['platform_family'] == 'rhel') &&
+     (ohai['platform_version'].to_f < 6.0)
+    configure << "--with-luajit-xcflags='-std=gnu99'"
+  end
+
+  command configure.join(" "), env: env
+
+  make "-j #{workers}", env: env
+  make "install", env: env
+
+end
