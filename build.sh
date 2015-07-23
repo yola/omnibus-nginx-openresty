8e5dcc08cdd341f0fa9445e1f3dcc55d40ba426f
Bearnard Hibbins <bearnard@gmail.com>
2015-07-23 11:39:27 +0200

    .ccache has the wrong permissions when omnibus gets installed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eux
 
-chown -R vagrant:vagrant /opt
 bundle install --without development
+chown -R vagrant:vagrant ~/.ccache
 bundle exec omnibus build openresty --override append_timestamp:false
